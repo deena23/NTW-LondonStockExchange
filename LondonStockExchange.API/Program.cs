7385c463c79b02c979d140de73b0e0e37aa1e3b7
deena <deena23032000@gmail.com>
2025-12-18 09:12:57 +0530

    Moved all code under namespace - Program.cs

--- LondonStockExchange.API/Program.cs
+++ LondonStockExchange.API/Program.cs
@@ -7,65 +7,74 @@ using Microsoft.IdentityModel.Tokens;
 using Serilog;
 using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Host.UseSerilog((context, configuration) =>
-    configuration.ReadFrom.Configuration(context.Configuration));
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var configuration = builder.Configuration.GetSection("ConfigurationItem");
-
-ConfigurationItem configurationItem = new();
-
-configuration.Bind(configurationItem);
-
-builder.Services.Configure<ConfigurationItem>(configuration);
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = configurationItem.JwTAuthentication.Issuer,
-            ValidAudience = configurationItem.JwTAuthentication.Audience,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configurationItem.JwTAuthentication.SecretKey))
-        };
-
-    });
-
-builder.Services.AddScoped<IStockService, StockService>();
-builder.Services.AddScoped<IStockRepository, StockRepository>();
-
-builder.Services.AddScoped<ITradeService, TradeService>();
-builder.Services.AddScoped<ITradeRepository, TradeRepository>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace LondonStockExchange
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Host.UseSerilog((context, configuration) =>
+                configuration.ReadFrom.Configuration(context.Configuration));
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var configuration = builder.Configuration.GetSection("ConfigurationItem");
+
+            ConfigurationItem configurationItem = new();
+
+            configuration.Bind(configurationItem);
+
+            builder.Services.Configure<ConfigurationItem>(configuration);
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configurationItem.JwTAuthentication.Issuer,
+                        ValidAudience = configurationItem.JwTAuthentication.Audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configurationItem.JwTAuthentication.SecretKey))
+                    };
+
+                });
+
+            builder.Services.AddScoped<IStockService, StockService>();
+            builder.Services.AddScoped<IStockRepository, StockRepository>();
+
+            builder.Services.AddScoped<ITradeService, TradeService>();
+            builder.Services.AddScoped<ITradeRepository, TradeRepository>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseMiddleware<ExceptionMiddleware>();
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
