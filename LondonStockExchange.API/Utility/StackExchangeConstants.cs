7f2980226431197d1885b4aea366315db30ed092
deena <deena23032000@gmail.com>
2025-12-18 09:05:06 +0530

    Added region for respective items

--- LondonStockExchange.API/Utility/StackExchangeConstants.cs
+++ LondonStockExchange.API/Utility/StackExchangeConstants.cs
@@ -4,10 +4,13 @@ namespace LondonStockExchange.Utility
 {
     public static class StackExchangeConstants
     {
+        #region Stored Procedure
         public static readonly string SP_InsertTradeDetails = "[StockExchange].[InsertTradeDetails]";
         public static readonly string SP_GetStockPriceByTicker = "[StockExchange].[GetStockPriceByTicker]";
         public static readonly string SP_GetStockPriceForTickers = "[StockExchange].[GetStockPriceForTickers]";
+        #endregion
 
+        #region Enum Validation
         public enum TradeDetailsValidationMessage
         {
             [Description("Trade details cannot be null.")]
@@ -21,11 +24,16 @@ namespace LondonStockExchange.Utility
             [Description("Broker ID is mandatory.")]
             BrokerId
         }
+        #endregion
 
+        #region Common Validation Messages
         public static readonly string TickerMandatoryMessage = "Ticker should not be empty.";
         public static readonly string StockPriceNotFoundMessageForTicker = "Stock price was not found for: {0}";
         public static readonly string NoStockFoundMessage = "No stock found";
+        #endregion
 
+        #region UDT
         public static readonly string UDT_SearchRequest = "[StockExchange].[SearchRequest]";
+        #endregion
     }
 }
